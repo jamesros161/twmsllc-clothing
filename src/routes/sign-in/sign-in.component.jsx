45d6afd923337fb39e026b0ea8a9842f9be80ecf
James Rosado <jamesros@boldgrid.com>
2022-07-17 19:15:09 -0400

    added sign-up components

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,25 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth
+} from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import Button from '../../components/button/button.component';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
+        const { user }   = await signInWithGooglePopup();
+        const userDocRef = createUserDocumentFromAuth(user);
     }
+
     return (
         <div>
             <h2>Sign In Page</h2>
-            <button
+            <Button buttonType="google"
                 onClick={logGoogleUser}>
                 Sign in with Google Popup
-            </button>
+            </Button>
+            <SignUpForm />
         </div>
     )
 }
